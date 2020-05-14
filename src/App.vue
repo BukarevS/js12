5bb4f3874c98b704de187f0d301a88588f3db07e
BukarevS <62722520+BukarevS@users.noreply.github.com>
2020-05-14 18:11:31 +0300

    2commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <contacts></contacts>
+  </div>
+</template>
+
+<script>
+import Contacts from './components/contacts.vue'
+
+export default {
+  name: 'App',
+  components: {
+    contacts: Contacts
+  },
+  data(){
+    return{
+    
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
